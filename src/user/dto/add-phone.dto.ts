44d282ed9d03834b6c163095b7ed268361f78011
Ulvy <romy.ulvy@passapptech.com.kh>
2026-07-28 21:10:51 +0700

    fix(phone): specify country code for phone number validation

--- src/user/dto/add-phone.dto.ts
+++ src/user/dto/add-phone.dto.ts
@@ -2,6 +2,6 @@ import { IsNotEmpty, IsPhoneNumber } from 'class-validator';
 
 export class AddPhoneDto {
   @IsNotEmpty()
-  @IsPhoneNumber(undefined, { message: 'phone must be a valid phone number' })
+  @IsPhoneNumber('KH', { message: 'phone must be a valid phone number' })
   phoneNumber: string;
 }
