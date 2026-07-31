098b3af8d74b9755cef26946e4002b2a67b79b89
Ulvy <romy.ulvy@passapptech.com.kh>
2026-07-31 09:23:00 +0700

    feat(i18n): update Khmer error messages for clarity and consistency feat(user): add length validation for phone number in AddPhoneDto

--- src/user/dto/add-phone.dto.ts
+++ src/user/dto/add-phone.dto.ts
@@ -1,7 +1,8 @@
-import { IsNotEmpty, IsPhoneNumber } from 'class-validator';
+import { IsNotEmpty, IsPhoneNumber, Length } from 'class-validator';
 
 export class AddPhoneDto {
   @IsNotEmpty()
+  @Length(8, 11, { message: 'phone number must be between 8 and 11 digits' })
   @IsPhoneNumber('KH', { message: 'phone must be a valid phone number' })
   phoneNumber: string;
 }
